88ca620ead0726f7ba2b3d15b8c949c3c044c94a
Jordan <knightworks@pm.me>
2020-01-16 18:02:35 -0600

    added package.json

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let {auth, ajx, currentUser } = require('./frontend')
-let jsonp = require('./jsonp')
+let {auth, ajx, currentUser } = require('./front')
+let jsonp = require('.lib/jsonp')
 
 module.exports = {
     auth,
